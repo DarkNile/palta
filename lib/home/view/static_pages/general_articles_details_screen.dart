98849e972e717bef09ae58cccb263fc9b077a4ca
Mahmoud Hamed <Mahmoud-M-Hamed@users.noreply.github.com>
2023-07-11 09:36:49 +0300

    create general articles details screen

---
+++ lib/home/view/static_pages/general_articles_details_screen.dart
@@ -0,0 +1,85 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/utils.dart';
+import 'package:palta/constants/extensions.dart';
+import 'package:palta/widgets/custom_app_bar_clip_path.dart';
+import 'package:palta/widgets/custom_loading_widget.dart';
+import 'package:palta/widgets/custom_text.dart';
+
+class GeneralArticlesDetailsScreen extends StatelessWidget {
+  const GeneralArticlesDetailsScreen(
+      {Key? key,
+      required this.imageUrl,
+      required this.dateCreated,
+      required this.description,
+      required this.name})
+      : super(key: key);
+
+  final String imageUrl;
+  final String name;
+  final String description;
+  final String dateCreated;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipPath(
+                  clipper: CustomAppBarClipPath(),
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 300,
+                    placeholder: (context, url) {
+                      return const CustomLoadingWidget();
+                    },
+                  ),
+                ),
+                Positioned(
+                  left: 20,
+                  top: 40,
+                  child: customBackButton(),
+                ),
+              ],
+            ),
+            40.ph,
+            CustomText(
+              text: name.tr,
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+            ),
+            5.ph,
+            CustomText(
+              text: dateCreated,
+              fontWeight: FontWeight.w500,
+              fontSize: 14,
+            ),
+            15.ph,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              ),
+              child: CustomText(
+                text: description.tr.split('<br>').join().trim(),
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customBackButton() => const CircleAvatar(
+        backgroundColor: Colors.white,
+        radius: 20,
+        child: Icon(Icons.chevron_right, color: Colors.black),
+      );
+}
