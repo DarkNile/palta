87805de0036e3c0154e7b5c8ba86ab3c580bd839
Mahmoud Hamed <Mahmoud-M-Hamed@users.noreply.github.com>
2023-07-12 18:43:40 +0300

    add rating bottom sheet

---
+++ lib/home/view/static_pages/rating_bottom_sheet.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:get/get.dart';
+import 'package:palta/constants/extensions.dart';
+import 'package:palta/home/controllers/home_controller.dart';
+import 'package:palta/home/models/review.dart';
+import 'package:palta/widgets/custom_outlined_button.dart';
+import 'package:palta/widgets/custom_text.dart';
+import 'package:palta/widgets/custom_text_field.dart';
+
+
+class RatingBottomSheetBuilder extends StatefulWidget {
+  const RatingBottomSheetBuilder(
+      {Key? key, required this.isGuest, required this.customerId})
+      : super(key: key);
+  final bool isGuest;
+  final String customerId;
+
+  @override
+  State<RatingBottomSheetBuilder> createState() =>
+      _RatingBottomSheetBuilderState();
+}
+
+class _RatingBottomSheetBuilderState extends State<RatingBottomSheetBuilder> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  TextEditingController guestNameController = TextEditingController();
+
+  TextEditingController reviewTextController = TextEditingController();
+  HomeController homeController = Get.put(HomeController());
+  late int rating;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery
+            .of(context)
+            .viewInsets
+            .bottom,
+      ),
+      child: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                (widget.isGuest)
+                    ? Container()
+                    : CustomTextField(
+                  controller: guestNameController,
+                  labelText: 'guestName'.tr,
+                  validator: true,
+                  prefixIcon: const Icon(Icons.person_outline),
+                ),
+                20.ph,
+                CustomTextField(
+                  controller: reviewTextController,
+                  hintText: 'review'.tr,
+                  maxLines: 6,
+                  validator: true,
+                ),
+                20.ph,
+                Row(
+                  children: [
+                    CustomText(
+                      text: 'rating'.tr,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    const CustomText(
+                      text: ':',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                    RatingBar.builder(
+                      initialRating: 3,
+                      minRating: 1,
+                      tapOnlyMode: true,
+                      direction: Axis.horizontal,
+                      allowHalfRating: false,
+                      itemCount: 5,
+                      itemSize: 25,
+                      itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      itemBuilder: (context, _) =>
+                      const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      onRatingUpdate: (rating) {
+                        this.rating = rating.toInt();
+                        print(rating);
+                      },
+                    ),
+                  ],
+                ),
+                20.ph,
+                CustomOutlinedButton(
+                  title: 'addReview'.tr,
+                  onPressed: () {
+                    homeController.postReviews(blogId: '2'
+                      // customerId
+                      ,
+                      reviewModel: ReviewModel(
+                          customerName: guestNameController.text, reviewText: reviewTextController.text,
+                          rating: rating),);
+                  },
+                ),
+                20.ph,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
