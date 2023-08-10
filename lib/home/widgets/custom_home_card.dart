7ef2a49f4cc9100ad02165f029ffa205952ad4e9
DarkNile <amgad_hesham@outlook.com>
2023-08-10 13:45:03 +0300

    UI Updates

--- lib/home/widgets/custom_home_card.dart
+++ lib/home/widgets/custom_home_card.dart
@@ -16,37 +16,40 @@ class CustomHomeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      color: Colors.white,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(6)),
-      ),
-      child: Container(
-        width: 176,
-        height: 140,
-        padding: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(6)),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(child: SvgPicture.asset('assets/icons/$icon.svg')),
-            Expanded(
-              child: CustomText(
-                text: title.tr,
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-                textOverflow: TextOverflow.visible,
-              ),
+    return
+        // Card(
+        //   elevation: 0,
+        //   color: Colors.white,
+        //   shape: const RoundedRectangleBorder(
+        //     borderRadius: BorderRadius.all(Radius.circular(6)),
+        //   ),
+        //   child:
+        Container(
+      // width: 176,
+      height: 140,
+      // padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      // decoration: const BoxDecoration(
+      //   color: Colors.white,
+      //   borderRadius: BorderRadius.all(Radius.circular(6)),
+      // ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(child: SvgPicture.asset('assets/icons/$icon.svg')),
+          Expanded(
+            child: CustomText(
+              text: title.tr,
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              textOverflow: TextOverflow.visible,
             ),
-            40.ph,
-          ],
-        ),
+          ),
+          40.ph,
+        ],
       ),
+      // ),
     );
   }
 }
